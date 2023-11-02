dc185a58752a066590cfb8a075d09fe9c372cc12
afir123 <kiri15@o2.pl>
2023-11-02 13:36:23 +0100

    zadanie v7

--- src/main/java/pl/javastart/task/shop/SalesDepartment.java
+++ src/main/java/pl/javastart/task/shop/SalesDepartment.java
@@ -8,13 +8,14 @@ public class SalesDepartment {
 
     @SuppressWarnings("EnhancedSwitchMigration")
     public double grossPrice(Product product) {
-        switch (product.category) {
+        switch (product.getCategory()) {
+//wykorzystano product.getCategory() zamiast product.category w celu uniknięcia błędu gdy category=null
             case "Nabiał":
-                return product.getNetPrice() * (1+vat1);
+                return product.getNetPrice() * (1 + vat1);
             case "Słodycze":
-                return product.getNetPrice() * (1+vat2);
-            case null, default:
-                return product.getNetPrice() * (1+vat3);
+                return product.getNetPrice() * (1 + vat2);
+            default:
+                return product.getNetPrice() * (1 + vat3);
         }
     }
 }
